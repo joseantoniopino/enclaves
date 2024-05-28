834680ef07d918ed5b18042f6a04018d674f5101
Jose Pino <josanpino@gmail.com>
2024-05-29 01:17:19 +0200

    feat(roll) Fixes in dock block

--- src/BoundedContext/Shared/Main/Roll/Application/Responses/RollResponse.php
+++ src/BoundedContext/Shared/Main/Roll/Application/Responses/RollResponse.php
@@ -4,6 +4,10 @@ namespace Src\BoundedContext\Shared\Main\Roll\Application\Responses;
 
 class RollResponse
 {
+    /**
+     * @param int $total
+     * @param string[] $details
+     */
     public function __construct(
         public readonly int $total,
         public readonly array $details,
